552e2cc58f8a4ab9f3deaea616c4626c117f68a6
Daniel Bok <daniel.bok@outlook.com>
2023-09-06 18:48:00 +0800

    refactor map design

--- src/components/EventDetail/LocationMap.tsx
+++ src/components/EventDetail/LocationMap.tsx
@@ -1,5 +1,5 @@
 import { CaretDownOutlined, CaretRightOutlined } from "@ant-design/icons";
-import { Alert, Collapse } from "antd";
+import { Collapse } from "antd";
 import React, { useState } from "react";
 import styles from "./styles.module.css";
 
@@ -29,7 +29,7 @@ const LocationMap = () => {
           children: (
             <iframe
               title="Villa Latitude Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3942.384730429034!2d115.18186437618937!3d-8.843725890561638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd25b6343123d25%3A0xbcc777a8bded2e6e!2sVilla%20Latitude%20Bali!5e0!3m2!1sen!2ssg!4v1693996162237!5m2!1sen!2ssg"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63085.95186774473!2d115.1404939849599!3d-8.798095139648765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd25b6343123d25%3A0xbcc777a8bded2e6e!2sVilla%20Latitude%20Bali!5e0!3m2!1sen!2ssg!4v1693997197332!5m2!1sen!2ssg"
               className={styles.mapContainer}
               allowFullScreen
               loading="lazy"
